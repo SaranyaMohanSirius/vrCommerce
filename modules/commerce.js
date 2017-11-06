2b723a73dce1414b0d482abf7a435d33f89f10d8
Monica Ramireddi <Monica.Ramireddi@siriuscom.com>
2017-11-06 17:46:06 +0530

    initial project setup

---
+++ modules/commerce.js
@@ -0,0 +1,17 @@
+var constants = require('./constants');
+var util = require('./util');
+var request = require('request');
+var database = require('./databaseUtil');
+var q = require('q');
+var gampee = require("gampee");
+var rp = require('request-promise');
+var Promise = require('promise');
+
+var _ = require("underscore");
+
+var globalcount = 0;
+module.exports = {
+
+
+
+};
---
+++ modules/databaseUtil.js
@@ -0,0 +1,15 @@
+var constants = require('./constants');
+var util = require('./util');
+var q = require('q');
+//lets require/import the mongodb native drivers.
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = require('mongodb').MongoClient;
+var database;
+
+// Use connect method to connect to the Server
+// Init method
+
+
+module.exports = {
+
+}
