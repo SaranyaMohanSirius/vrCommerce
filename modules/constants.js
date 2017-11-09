4a4d8313d7744e19b2b69e8720d3fe7d57bb0669
Veeraraghavan Ramachandran <veeraraghavan.ramachandran@siriuscom.com>
2017-11-09 11:31:00 +0530

    Added guest login for EP on the start - constant file change

--- modules/constants.js
+++ modules/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  "EP_HOSTNAME" : "localhost:9080",
+  "EP_HOSTNAME" : "b96dc244.ngrok.io",
   "EP_GUEST_LOGIN" : "/cortex/oauth2/tokens?grant_type=password&role=PUBLIC&scope=mobee",
   
   "HTML_DIR" : "WebContent/html/"
