951bcc44171708e98e5926cc960b2adb8ff0d293
Veeraraghavan Ramachandran <Veeraraghavan.Ramachandran@siriuscom.com>
2017-11-15 20:06:22 +0530

    Top categories - britney

--- modules/constants.js
+++ modules/constants.js
@@ -3,7 +3,7 @@ module.exports = {
   "EP_HOSTNAME" : "53868ff2.ngrok.io",
   "EP_HOSTNAME_CORTEX" : "53868ff2.ngrok.io/cortex",
   "EP_GUEST_LOGIN" : "/cortex/oauth2/tokens?grant_type=password&role=PUBLIC&scope=britney",
-  "EP_TOP_CATEGORIES" : "/cortex/navigations/mobee?zoom=element",
+  "EP_TOP_CATEGORIES" : "/cortex/navigations/britney?zoom=element",
   "EP_ACCESS_TOKEN" : "ff2d67d2-27d7-4872-9945-341e403de818",
   "EP_SEARCH_ZOOM" : "?zoom=element:availability,element:code,element:definition,element:element,element:price",
   "EP_SEARCH" : "/cortex/searches/britney/keywords/items?followlocation",
