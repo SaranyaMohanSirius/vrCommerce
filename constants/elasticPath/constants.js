1df9a89262e7907778decefb1a0649c5278aca13
Joseph <josephpeter.john@siriuscom.com>
2017-11-16 11:49:12 +0530

    Code Base Node JS Folder Structures

---
+++ constants/elasticPath/constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  "EP_HOSTNAME" : "53868ff2.ngrok.io",
+  "EP_HOSTNAME_CORTEX" : "53868ff2.ngrok.io/cortex",
+  "EP_GUEST_LOGIN" : "/cortex/oauth2/tokens?grant_type=password&role=PUBLIC&scope=britney",
+  "EP_TOP_CATEGORIES" : "/cortex/navigations/britney?zoom=element",
+  "EP_ACCESS_TOKEN" : "69d0ead7-4e6d-40f0-bcde-8cbaa8924740",
+  "EP_SEARCH_ZOOM" : "?zoom=element:availability,element:code,element:definition,element:element,element:price",
+  "EP_SEARCH" : "/cortex/searches/britney/keywords/items?followlocation", 
+  "HTML_DIR" : "WebContent/html/"
+};
