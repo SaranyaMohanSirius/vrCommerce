8181775dc596a6bb5bc01e3fa3726b96ff2c1390
Joseph <josephpeter.john@siriuscom.com>
2017-11-24 13:33:14 +0530

    WCS: Removed unused Constants for Build Retrigger.

--- constants/wcs/constants.js
+++ constants/wcs/constants.js
@@ -1,7 +1,6 @@
 module.exports = {
 
   "WCS_HOSTNAME" : "34.194.115.169:3737",
-  "EP_STORE" : "britney",
   "WCS_AWS_IMAGE_PATH" : "http://s3.ap-south-1.amazonaws.com/sirius-ep-images/",
   "WCS_IMAGE_FMT": ".jpg",
   "WCS_PRODUCT_DETAILS": "/search/resources/store/",
