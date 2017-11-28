6f7549daf5d1c6070d122026a31492563e61d4fe
Joseph <josephpeter.john@siriuscom.com>
2017-11-28 12:40:55 +0530

    EP & WCS : Moved Routes code to Route index Js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var cron = require('node-cron');
+var util = require('./util/elasticPath/util');
+var logger= util.getLogger();
 
 var app = express();
 
@@ -16,35 +18,14 @@ app.use(bodyParser.urlencoded({
 // Process application/json
 app.use(bodyParser.json());
 
-//Define Routes Here  
-var epLoginRoute = require('./routes/elasticPath/loginRoutes');
-var epSearchRoute = require('./routes/elasticPath/searchRoutes');
-var epCategoryRoute = require('./routes/elasticPath/categoryRoutes');
-var epPDPRoute = require('./routes/elasticPath/pdpRoutes');
-var epCartRoute = require('./routes/elasticPath/cartRoutes');
-var epShipModeRoute = require('./routes/elasticPath/shipModeRoutes');
-var epAddressRoute = require('./routes/elasticPath/addressRoutes');
-
-var wcsPDPRoute = require('./routes/wcs/pdpRoutes');
-var wcsCategoryRoute = require('./routes/wcs/categoryRoutes');
-var wcsShipModeRoutes = require('./routes/wcs/shipModeRoutes');
-
-//This needs to be  refined based on the discussion with UI Team
-app.use('/ep/login',epLoginRoute);
-app.use('/ep/search',epSearchRoute);
-app.use('/ep/category',epCategoryRoute);
-app.use('/ep/PDP',epPDPRoute);
-app.use('/ep/cart',epCartRoute);
-app.use('/ep/shipModes',epShipModeRoute);
-app.use('/ep/address',epAddressRoute);
-
-app.use('/wcs/PDP',wcsPDPRoute);
-app.use('/wcs/category',wcsCategoryRoute);
-app.use('/wcs/shipModes',wcsShipModeRoutes);
+/**
+ * App includes all the Routes
+ */
+require('./routes/index')(app);
 
 // Spin up the server
 app.listen(app.get('port'), function() {
-  console.log('running on port', app.get('port'))
+  logger.info('running on port', app.get('port'))
 })
 
 
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+//Elastic Path Routes
+var epLoginRoute = require('./elasticPath/loginRoutes');
+var epSearchRoute = require('./elasticPath/searchRoutes');
+var epCategoryRoute = require('./elasticPath/categoryRoutes');
+var epPDPRoute = require('./elasticPath/pdpRoutes');
+var epCartRoute = require('./elasticPath/cartRoutes');
+var epShipModeRoute = require('./elasticPath/shipModeRoutes');
+var epAddressRoute = require('./elasticPath/addressRoutes');
+
+//Websphere Commerce Routes
+var wcsPDPRoute = require('./wcs/pdpRoutes');
+var wcsCategoryRoute = require('./wcs/categoryRoutes');
+var wcsShipModeRoutes = require('./wcs/shipModeRoutes');
+
+module.exports = function (app) {
+    app.use('/ep/login',epLoginRoute);
+    app.use('/ep/search',epSearchRoute);
+    app.use('/ep/category',epCategoryRoute);
+    app.use('/ep/PDP',epPDPRoute);
+    app.use('/ep/cart',epCartRoute);
+    app.use('/ep/shipModes',epShipModeRoute);
+    app.use('/ep/address',epAddressRoute);
+
+    app.use('/wcs/PDP',wcsPDPRoute);
+    app.use('/wcs/category',wcsCategoryRoute);
+    app.use('/wcs/shipModes',wcsShipModeRoutes);
+};
+
+//This needs to be  refined based on the discussion with UI Team
+
+
