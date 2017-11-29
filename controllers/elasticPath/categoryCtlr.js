afbcaa75b805c497df15bdfd2bc2ac5f4167361a
Veeraraghavan Ramachandran <Veeraraghavan.Ramachandran@siriuscom.com>
2017-11-29 11:59:02 +0530

    Request Promise change for categoryCtrl in EP

--- controllers/elasticPath/categoryCtlr.js
+++ controllers/elasticPath/categoryCtlr.js
@@ -2,6 +2,7 @@ var constants = require('../../constants/elasticPath/constants');
 var util = require('../../util/elasticPath/util');
 var categoryMapper = require('../../json_mappers/elasticPath/categoryMapper');
 var request = require('request');
+var requestPromise = require('request-promise');
 
 
 var logger= util.getLogger();
@@ -12,116 +13,84 @@ module.exports = {
   /*Controller for getting the Top level categories for header in EP*/ 
   getTopCategories: function(token,res){
     
-  messageData = {};
+      messageData = {};
 
-  logger.info('Top categories EP url: ', util.constructUrl(constants.EP_HOSTNAME, constants.EP_TOP_CATEGORIES, false));
-  
-  request({
-      url: util.constructUrl(constants.EP_HOSTNAME, constants.EP_TOP_CATEGORIES, false),
-      method: 'GET',
-      json: messageData,
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'bearer ' + token
-      },
-    }, function(error, response, body) {
-      if (!error) {
-        if (!body.errors) {
-      
-          var result = categoryMapper.mapTopCategoriesJSON(body);
-          res.send({
-                      "success": true ,
-                      "result": result,
-            });
+      var topCategoriesURL = util.constructUrl(constants.EP_HOSTNAME, constants.EP_TOP_CATEGORIES, false);   
+      logger.info('Top categories EP url: ', topCategoriesURL);
+      var method ='GET';
+      var requestCall = util.constructRequest(topCategoriesURL,method,messageData,token);
 
-        } else {
-            logger.error('errors in service to get Top categories in EP: ', body.errors);
-            res.send({ "success": false, "error": body.errors });
-        }
-      } else {
-            logger.error('errors in service to get Top categories in EP: ', error);
-            res.send({ "success": false, "error": error });
-      }
-    }); 
+      requestPromise(requestCall).then(function (data) {
+         var result = categoryMapper.mapTopCategoriesJSON(data); 
+              res.send({
+                "success": true ,
+                "result": result,                                            
+            });   
+      }).catch(function (error) {
+            if(error.response.body){
+              logger.error('errors in service to get Top categories in EP: ', error.response.body);
+              res.send({ "success": false, "error": error.response.body }); 
+            }else{
+              logger.error('errors in service to get Top categories in EP: ', error);
+              res.send({ "success": false, "error": error});
+            }
+      });
   },
 
   /*Controller for getting the Sub categories for header nav menu in EP*/
   getSubCategories: function(token,res,identifier){
     
-  messageData = {};
-  var concattUrl =  identifier + constants.EP_SUB_CATEGORIES_ZOOM;
-
-  logger.info('Sub Categories EP url:' , util.constructUrl(constants.EP_HOSTNAME_CORTEX, concattUrl, false));
+      messageData = {};
+      var concattUrl =  identifier + constants.EP_SUB_CATEGORIES_ZOOM;
+      var subCategoriesURL = util.constructUrl(constants.EP_HOSTNAME_CORTEX, concattUrl, false);
+      logger.info('Sub Categories EP url:' , subCategoriesURL);
+      var method ='GET';
+      var requestCall = util.constructRequest(subCategoriesURL,method,messageData,token);
   
-  request({
-      url: util.constructUrl(constants.EP_HOSTNAME_CORTEX, concattUrl, false),
-      method: 'GET',
-      json: messageData,
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'bearer ' + token
-      },
-    }, function(error, response, body) {
-      if (!error) {
-        if (!body.errors) { 
-      
-            var result = categoryMapper.mapSubCategoriesJSON(body,identifier);
-            res.send({
-                      "success": true ,
-                      "result": result,
-            });
+      requestPromise(requestCall).then(function (data) {
+         var result = categoryMapper.mapSubCategoriesJSON(data,identifier); 
+              res.send({
+                "success": true ,
+                "result": result,                                            
+            });   
+      }).catch(function (error) {
+            if(error.response.body){
+              logger.error('errors in service to get Sub categories in EP:', error.response.body);
+              res.send({ "success": false, "error": error.response.body }); 
+            }else{
+              logger.error('errors in service to get Sub categories in EP:', error);
+              res.send({ "success": false, "error": error});
+            }
+      });
 
-        } else {
-            logger.error('errors in service to get Sub categories in EP: ', body.errors);
-            res.send({ "success": false, "error": body.errors });
-        }
-      } else {
-            logger.error('errors in service to get Sub categories in EP: ', error);
-            res.send({ "success": false, "error": error });
-      }
-    }); 
   },
 
   /*Controller for getting the products list for a given category/subcategory in EP*/
   getProductsListForCategory: function(token,res,identifier){
 
-    messageData = {};
-  
-    var n = identifier.split("/");
-    var categoryIdentifier = n[n.length - 1];
+      messageData = {};
+      var n = identifier.split("/");
+      var categoryIdentifier = n[n.length - 1];
+      var concatURL =  constants.EP_PRODUCTS_FROM_CATEGORIES_NAV + categoryIdentifier + constants.EP_SEARCH_ZOOM;
+      var productListUrl = util.constructUrl(constants.EP_HOSTNAME_CORTEX, concatURL, false);
+      logger.info('Categories page Product List EP url:' , productListUrl);
+      var method ='GET';
+      var requestCall = util.constructRequest(productListUrl,method,messageData,token);
 
-    var concatURL =  constants.EP_PRODUCTS_FROM_CATEGORIES_NAV + categoryIdentifier + constants.EP_SEARCH_ZOOM;
-
-    var productListUrl = util.constructUrl(constants.EP_HOSTNAME_CORTEX, concatURL, false);
-
-    logger.info('Categories page Product List EP url:' , productListUrl);
-
-    request({
-      url: productListUrl,
-      method: 'GET',
-      json: messageData,
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'bearer ' + token
-      },
-    }, function(error, response, body) {
-        if (!error) {
-              if(!body.errors){
-
-                  var result = categoryMapper.mapProductsListForCategoryJSON(body);
-                  res.send({
-                    "success": true ,
-                    "result": result,                                            
-                  });                            
-              }
-              else{
-                logger.error('errors in service to get products list for category in EP: ', body.errors);
-                res.send({ "success": false, "error": body.errors });
-              }
-        }else{
-                logger.error('errors in service to get products list for category in EP: ', body.errors);
-                res.send({ "success": false, "error": error });                        
-        }
-    });
+      requestPromise(requestCall).then(function (data) {
+         var result = categoryMapper.mapProductsListForCategoryJSON(data); 
+              res.send({
+                "success": true ,
+                "result": result,                                            
+            });   
+      }).catch(function (error) {
+            if(error.response.body){
+              logger.error('errors in service to get products list for category in EP: ', error.response.body);
+              res.send({ "success": false, "error": error.response.body }); 
+            }else{
+              logger.error('errors in service to get products list for category in EP: ', error);
+              res.send({ "success": false, "error": error});
+            }
+      });
   }
 };
