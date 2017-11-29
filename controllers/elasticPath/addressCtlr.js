da96f887a5f2faac9954ab7cf4809f354c66dc65
Veeraraghavan Ramachandran <Veeraraghavan.Ramachandran@siriuscom.com>
2017-11-29 11:30:57 +0530

    Request Promise change for addressCtrl done

--- controllers/elasticPath/addressCtlr.js
+++ controllers/elasticPath/addressCtlr.js
@@ -3,6 +3,7 @@ var constants = require('../../constants/elasticPath/constants');
 var util = require('../../util/elasticPath/util');
 var addressMapper = require('../../json_mappers/elasticPath/addressMapper');
 var request = require('request');
+var requestPromise = require('request-promise');
 
 
 var logger= util.getLogger();
@@ -28,33 +29,26 @@ module.exports = {
       };
       var addShippingAddressURL = util.constructUrl(constants.EP_HOSTNAME, constants.EP_ADD_SHIPPING_ADDRESS, false);   
       logger.info('addShippingAddress url',  addShippingAddressURL);
-      request({
-        url: addShippingAddressURL,
-        method: 'POST',
-        json: messageData,
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'bearer ' + token
-        },
-      }, function(error, response, body) {
-            if (!error) {
-                if(!body.errors){
-                  var result = addressMapper.addShippingAddressJSON();  
-                  res.send({
-                    "success": true ,
-                    "result": result,
-                  });                           
-                }
-                else{
-                  logger.error('errors in service to addShippingAddress in EP: ', body.errors);                
-                  res.send({ "success": false, "error": body.errors });
-                }
-            }else{
-                logger.error('errors in service to addShippingAddress in EP: ', error);
-                res.send({ "success": false, "error": error });                        
-            }
+      var method ='POST';
+      var requestCall = util.constructRequest(addShippingAddressURL,method,messageData,token)
 
-        });
+      requestPromise(requestCall).then(function (data) {
+         var result = addressMapper.addShippingAddressJSON(data); 
+              res.send({
+                "success": true ,
+                "result": result,                                            
+            });   
+      }).catch(function (error) {
+            if(error.response.body){
+              logger.error('errors in service to addShippingAddress in EP: ', error.response.body);
+              res.send({ "success": false, "error": error.response.body }); 
+            }else{
+              logger.error('errors in service to addShippingAddress in EP: ', error);
+              res.send({ "success": false, "error": error});
+            }
+      });
+
+
   },
 
   /*Controller for getting all the shipping address in EP*/
@@ -65,35 +59,24 @@ module.exports = {
       var conCatUrl = constants.EP_DEFAULT_CART + constants.EP_GET_SHIPPING_ADDRESS_ZOOM;
       var getShippingAddressesURL = util.constructUrl(constants.EP_HOSTNAME, conCatUrl, false);   
       logger.info('getShippingAddresses url',  getShippingAddressesURL);
-      request({
-        url: getShippingAddressesURL,
-        method: 'GET',
-        json: messageData,
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'bearer ' + token
-        },
-      }, function(error, response, body) {
-            if (!error) {
-                if(!body.errors){
+      var method ='GET';
+      var requestCall = util.constructRequest(getShippingAddressesURL,method,messageData,token)
 
-                  var result = addressMapper.getShippingAddressesJSON(body);
-                  res.send({
-                    "success": true ,
-                    "result": result,
-                  });                           
-                }
-                else{
-                  logger.error('errors in service to addShippingAddress in EP: ', body.errors);                
-                  res.send({ "success": false, "error": body.errors });
-                }
+      requestPromise(requestCall).then(function (data) {
+         var result = addressMapper.getShippingAddressesJSON(data); 
+              res.send({
+                "success": true ,
+                "result": result,                                            
+            });   
+      }).catch(function (error) {
+            if(error.response.body){
+              logger.error('errors in service to get ShippingAddress in EP: ', error.response.body);
+              res.send({ "success": false, "error": error.response.body }); 
             }else{
-                logger.error('errors in service to addShippingAddress in EP: ', error);
-                res.send({ "success": false, "error": error });                        
+              logger.error('errors in service to get ShippingAddress in EP: ', error);
+              res.send({ "success": false, "error": error});
             }
-
-        });
-
+      });
   },
 
   /*Controller for deleting shipping address in EP*/
@@ -102,32 +85,24 @@ module.exports = {
       var uri= req.body.addressId;
       var deleteShippingAddressURL = util.constructUrl(constants.EP_HOSTNAME_CORTEX, uri, false);   
       logger.info('deleteShippingAddress url',  deleteShippingAddressURL);
-      request({
-        url: deleteShippingAddressURL,
-        method: 'DELETE',
-        json: messageData,
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'bearer ' + token
-        },
-      }, function(error, response, body) {
-            if(!error){
-              if (!response.body) {
-                   var result = addressMapper.deleteShippingAddressJSON(); 
-                    res.send({
-                      "success": true ,
-                      "result": result,                                            
-                    });                           
-                  }
-                  else{
-                    logger.error('errors in service to delete address in EP: ', response.body);
-                    res.send({ "success": false, "error": response.body });
-                  }  
-              }else{
-                  logger.error('errors in service to delete address in EP: ', error);
-                  res.send({ "success": false, "error": error });                        
-              }
-          });
+      var method ='DELETE';
+      var requestCall = util.constructRequest(deleteShippingAddressURL,method,messageData,token)
+
+      requestPromise(requestCall).then(function (data) {
+             var result = addressMapper.deleteShippingAddressJSON(data); 
+              res.send({
+                "success": true ,
+                "result": result,                                            
+            });   
+      }).catch(function (error) {
+            if(error.response.body){
+              logger.error('errors in service to delete address in EP: ', error.response.body);
+              res.send({ "success": false, "error": error.response.body }); 
+            }else{
+              logger.error('errors service to delete address in EP: ', error);
+              res.send({ "success": false, "error": error});
+            }
+      });
   },
 
   /*Controller for updating shipping address in EP*/
@@ -149,33 +124,26 @@ module.exports = {
       };
   		var uri = req.body.addressId;
   		var updateShippingAddressURL = util.constructUrl(constants.EP_HOSTNAME_CORTEX,uri,false);
-		logger.info('updateShippingAddress url',  updateShippingAddressURL);
-	    request({
-	        url: updateShippingAddressURL,
-	        method: 'PUT',
-	        json: messageData,
-	        headers: {
-	          'Content-Type': 'application/json',
-	          'Authorization': 'bearer ' + token
-	        },
-	      }, function(error, response, body) {
-	            if(!error){
-	              if (!response.body) {
-	                   var result = addressMapper.updateShippingAddressJSON();
-	                    res.send({
-	                      "success": true ,
-	                      "result": result,                                            
-	                    });                           
-	                  }
-	                  else{
-	                    logger.error('errors in service to update address in EP: ', response.body);
-	                    res.send({ "success": false, "error": response.body });
-	                  }  
-	              }else{
-	                  logger.error('errors in service to update address in EP: ', error);
-	                  res.send({ "success": false, "error": error });                        
-	              }
-	          });
+  		logger.info('updateShippingAddress url',  updateShippingAddressURL);
+      var method ='PUT';
+      var requestCall = util.constructRequest(updateShippingAddressURL,method,messageData,token)
+
+      requestPromise(requestCall).then(function (data) {
+             var result = addressMapper.updateShippingAddressJSON(data); 
+              res.send({
+                "success": true ,
+                "result": result,                                            
+            });   
+      }).catch(function (error) {
+            if(error.response.body){
+              logger.error('errors in service to update address in EP: ', error.response.body);
+              res.send({ "success": false, "error": error.response.body }); 
+            }else{
+              logger.error('errors in service to update address in EP: ', error);
+              res.send({ "success": false, "error": error});
+            }
+      });
+
   },
 
   /*Controller for selecting the shipping address in EP*/
@@ -188,54 +156,41 @@ module.exports = {
 
     var selectShippingAddressURL = util.constructUrl(constants.EP_HOSTNAME , conCatUrl, false);
     logger.info('selectShippingAddress url ', selectShippingAddressURL);
-    request({
-          url: selectShippingAddressURL,
-          method: 'GET',
-          json: messageData,
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'bearer ' + token
-          },
-        }, function(error, response, body) {
-              if(!error){
-                    var uri = body._order[0]._deliveries[0]._element[0]._destinationinfo[0]._selector[0].self.uri;
-                    var concatURL = uri  + addressId + constants.EP_FOLLOW_LOCATION;
-                    var shippingAddressSelectURL = util.constructUrl(constants.EP_HOSTNAME_CORTEX, concatURL, false);
-                    logger.info('shipping address select post url ', shippingAddressSelectURL);
-                    
-                    messageData = {};
-                    request({
-                      url: util.constructUrl(constants.EP_HOSTNAME_CORTEX, concatURL, false),
-                      method: 'POST',
-                      json: messageData,
-                      headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': 'bearer ' + token
-                      },
-                    }, function(error, response, body) {
-                            if (!error) {
-                                if(!body.errors){
-                                   var result = addressMapper.selectShippingAddressJSON();
-                                    res.send({
-                                      "success": true ,
-                                      "result": result,                                            
-                                    });                           
-                                }
-                                else{
-                                  logger.error('errors in service to getSearchResults in EP: ', body.errors);               
-                                  res.send({ "success": false, "error": body.errors });
-                                }
-                            }else{
-                                logger.error('errors in service to getSearchResults in EP: ', error);
-                                res.send({ "success": false, "error": error });                        
-                            }
-                        });  
+    var method ='GET';
+    var requestCall = util.constructRequest(selectShippingAddressURL,method,messageData,token)
+
+    requestPromise(requestCall).then(function (data) {
+        var uri = data._order[0]._deliveries[0]._element[0]._destinationinfo[0]._selector[0].self.uri;
+        var concatURL = uri  + addressId + constants.EP_FOLLOW_LOCATION;
+        var shippingAddressSelectURL = util.constructUrl(constants.EP_HOSTNAME_CORTEX, concatURL, false);
+        logger.info('shipping address select post url ', shippingAddressSelectURL);
+
+         messageData = {};
+         var secondRequestCall = util.constructRequest(shippingAddressSelectURL,"POST",messageData,token)
+         return requestPromise(secondRequestCall).then(function (data) {
+             var result = addressMapper.selectShippingAddressJSON();
+              res.send({
+                "success": true ,
+                "result": result,                                            
+              });                              
+            }).catch(function (error) {
+                if(error.response.body){
+                  logger.error('errors in service while selecting shipping address in EP: ', error.response.body);
+                  res.send({ "success": false, "error": error.response.body }); 
                 }else{
-                    logger.error('errors in service to get shipping address selector in EP: ', error);
-                    res.send({ "success": false, "error": error });                        
+                  logger.error('errors in service while selecting shipping address in EP: ', error);
+                  res.send({ "success": false, "error": error});
                 }
             });
-
+      }).catch(function (error) {
+            if(error.response.body){
+              logger.error('errors in service to get shipping address selector in EP: ', error.response.body);
+              res.send({ "success": false, "error": error.response.body }); 
+            }else{
+              logger.error('errors in service to get shipping address selector in EP: ', error);
+              res.send({ "success": false, "error": error});
+            }
+      });
 
   }      
 
