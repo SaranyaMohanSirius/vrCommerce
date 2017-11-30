40966e1e78dc6e3efffaa08199c17d91bf777ce0
Arun Shunmugam <Arun.ShunmugamJawahar@siriuscom.com>
2017-11-30 16:02:49 +0530

    EP PDP request promise change

--- controllers/elasticPath/pdpCtlr.js
+++ controllers/elasticPath/pdpCtlr.js
@@ -2,7 +2,7 @@ var constants = require('../../constants/elasticPath/constants');
 var util = require('../../util/elasticPath/util');
 var pdpMapper = require('../../json_mappers/elasticPath/pdpMapper');
 var request = require('request');
-
+var requestPromise = require('request-promise');
 
 var logger= util.getLogger();
 
@@ -13,36 +13,28 @@ module.exports = {
 	   
     var concatURL = uri + constants.EP_PDP_ZOOM;   
     logger.info("getProductDetails post form url:" + util.constructUrl(constants.EP_HOSTNAME_CORTEX, concatURL, false));	
-	  var messageData = {};
-    request({
-      url: util.constructUrl(constants.EP_HOSTNAME_CORTEX, concatURL, false),
-      method: 'GET',
-      json: messageData,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'bearer ' + token
-      },
-    }, function(error, response, body) {
-          if (!error) {
-            if (!body.errors) {
-				  var code = body._code[0].code;
-				  var imageName = (code.split("-")[0]).toUpperCase();
-				  imageName = imageName.replace(".", "-");
-				  var concatImageURL =  constants.EP_AWS_IMAGE_PATH + imageName + constants.EP_IMAGE_FMT;
-				  logger.info('Image URL: '+concatImageURL);
-				  var result = pdpMapper.mapPdpJSON(body,concatImageURL); 
-                  res.send({
-                    "success": true ,
-                    "result": result,                                            
-                  });                            
-
-			}
-            else {
-              logger.error('errors in service hit to login service'+body.errors);
-              res.send({ "success": false, "error": body.errors });
+	var messageData = {};
+    var method = 'GET';
+	var requestCall = util.constructRequest(util.constructUrl(constants.EP_HOSTNAME_CORTEX, concatURL, false),method,messageData,token);
+	requestPromise(requestCall).then(function (data) {
+		  var body = data;
+		  var code = body._code[0].code;
+		  var imageName = (code.split("-")[0]).toUpperCase();
+		  imageName = imageName.replace(".", "-");
+		  var concatImageURL =  constants.EP_AWS_IMAGE_PATH + imageName + constants.EP_IMAGE_FMT;
+		  logger.info('Image URL: '+concatImageURL);
+		  var result = pdpMapper.mapPdpJSON(body,concatImageURL); 
+		  res.send({
+			"success": true ,
+			"result": result,                                            
+		  });                            
+	}).catch(function (error) {
+			if(error.response.body){
+			  logger.error('errors in service hit to login service '+error.response.body);
+              res.send({ "success": false, "error": error.response.body });
             }
-          } else {
-            logger.error('commerce error'+ error);
+           else {
+            logger.error('errors in service hit to login service '+ error);
             res.send({ "success": false, "error": error });
           }
     });
