59691921f0a0df75fb4e4f31fa875f024dc02bbc
Arun Shunmugam <Arun.ShunmugamJawahar@siriuscom.com>
2017-12-04 12:49:32 +0530

    PDP WCS final commit

--- controllers/wcs/pdpCtlr.js
+++ controllers/wcs/pdpCtlr.js
@@ -1,30 +1,30 @@
-var constants = require('../../constants/wcs/constants');
-var util = require('../../util/wcs/util');
-var pdpMapper = require('../../json_mappers/wcs/pdpMapper');
-var request = require('request');
-var requestPromise = require('request-promise');
+import constants from '../../constants/wcs/constants';
+import util from '../../util/wcs/util';
+import pdpMapper from '../../json_mappers/wcs/pdpMapper';
+import request from 'request';
+import requestPromise from 'request-promise';
 
-var logger= util.getLogger();
+let logger = util.getLogger();
 
 module.exports = {
 
   /*Controller for getting the Product details to be displayed in the PDP page in WCS*/
    getProductDetails: function(res,productId){
 	   
-    var productDetailURL = constants.WCS_PRODUCT_DETAILS + constants.WCS_STORE_ID + constants.WCS_PRODUCT_DETAILS_APPEND + productId + "?catalogId=" + constants.WCS_CATALOG_ID + "&langId=" + constants.WCS_LANG_ID;
+    let productDetailURL = constants.WCS_PRODUCT_DETAILS + constants.WCS_STORE_ID + constants.WCS_PRODUCT_DETAILS_APPEND + productId + "?catalogId=" + constants.WCS_CATALOG_ID + "&langId=" + constants.WCS_LANG_ID;
     logger.info("getProductDetails post form url:" + util.constructUrl(constants.WCS_HOSTNAME, productDetailURL, false));	
-	var method ='GET';
-	var messageData = {};
-	var requestCall = util.constructRequestWithoutToken(util.constructUrl(constants.WCS_HOSTNAME, productDetailURL, false),method,messageData);
+	let method ='GET';
+	let messageData = {};
+	let requestCall = util.constructRequestWithoutToken(util.constructUrl(constants.WCS_HOSTNAME, productDetailURL, false),method,messageData);
 	logger.info("requestCAll " + requestCall);
 	requestPromise(requestCall).then(function (data) {
-		var body1 = data;
-		var invAvlURL = constants.WCS_REST_URL + constants.WCS_STORE_ID + constants.WCS_INV_AVL + productId;
+		let body1 = data;
+		let invAvlURL = constants.WCS_REST_URL + constants.WCS_STORE_ID + constants.WCS_INV_AVL + productId;
 		logger.info("getInvAvl post form url:" + util.constructUrl(constants.WCS_HOSTNAME_NOPORT, invAvlURL, false));	
-		var secondRequestCall = util.constructRequestWithoutToken(util.constructUrl(constants.WCS_HOSTNAME_NOPORT, invAvlURL, false),"GET",messageData);
+		let secondRequestCall = util.constructRequestWithoutToken(util.constructUrl(constants.WCS_HOSTNAME_NOPORT, invAvlURL, false),"GET",messageData);
 		logger.info("requestCAll " + secondRequestCall);
 		requestPromise(secondRequestCall).then(function (data) {
-			  var result = pdpMapper.mapPdpJSON(body1,data);
+			  let result = pdpMapper.mapPdpJSON(body1,data);
 			  res.send({
 				"success": true ,
 				"result": result,
