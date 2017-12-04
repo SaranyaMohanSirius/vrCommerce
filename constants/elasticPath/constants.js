6ebb87121d5cb361e821d3e129c019955e365c2e
Joseph <josephpeter.john@siriuscom.com>
2017-12-04 09:23:31 +0530

    EP: Constant change moved store name to common variable.

--- constants/elasticPath/constants.js
+++ constants/elasticPath/constants.js
@@ -1,55 +1,61 @@
-module.exports = {
+import storeConstants from './storeConstants';
 
-  "EP_HOSTNAME" : "4c00070e.ngrok.io",
-  "EP_HOSTNAME_CORTEX" : "4c00070e.ngrok.io/cortex",
-  "EP_STORE" : "britney",
-  "EP_GUEST_LOGIN" : "/cortex/oauth2/tokens?grant_type=password&role=PUBLIC&scope=britney",
-  "EP_TOP_CATEGORIES" : "/cortex/navigations/britney?zoom=element",
+let storeName = storeConstants.EP_STORE;
+
+export default {
+
+  "EP_HOSTNAME" : storeConstants.EP_HOSTNAME,
+  "EP_HOSTNAME_CORTEX" : storeConstants.EP_HOSTNAME_CORTEX,
+  "EP_STORE" : storeName,
+  "EP_LOG_DIR" : "log/elasticPath/trace.log",
+
+  //All Definite Url Contansts Goes Here
+  "EP_GUEST_LOGIN" : "/cortex/oauth2/tokens?grant_type=password&role=PUBLIC&scope="+storeName,
+  "EP_TOP_CATEGORIES" : "/cortex/navigations/"+storeName+"?zoom=element",
+  "EP_PRODUCTS_FROM_CATEGORIES_NAV" : "/searches/"+storeName+"/navigations/items/",
+  "EP_SEARCH" : "/cortex/searches/"+storeName+"/keywords/items?followlocation",
+  "EP_ADD_SHIPPING_ADDRESS" : "/cortex/addresses/"+storeName+"?followlocation",  
+  "EP_DEFAULT_CART" : "/cortex/carts/"+storeName+"/default",
+  "EP_SHOPPING_CART" : "/carts/"+storeName+"/default",
+  "EP_CART" : "/carts/"+storeName+"/default",
+  "EP_CURRENT_ORDER" : "/orders/"+storeName+"/g5qwknzvgq4wkljqgfrtaljuga4dgllbmqytcljqmjstsntbg5rtqojsgq=",  
+  "EP_WHISHLIST_URL" : "wishlists/"+storeName+"/default" ,
+
+
+
+  // All additional params for Url Goes here
+  "EP_GET_BILLING_ADDRESS_SELECTOR": "/billingaddressinfo/selector",
+  "EP_FOLLOW_LOCATION": "?followlocation",
+  "EP_WHISHLIST": "/wishlists",
+  "HTML_DIR" : "WebContent/html/",
+  "EP_FORM": "/form",
+  "EP_AWS_IMAGE_PATH" : "http://s3.ap-south-1.amazonaws.com/sirius-ep-images/",
+  "EP_IMAGE_FMT": ".jpg",
+
+
+  // All Zoom Constant Goes Here
   "EP_SUB_CATEGORIES_ZOOM" : "?zoom=child",
-  "EP_PRODUCTS_FROM_CATEGORIES_NAV" : "/searches/britney/navigations/items/",
-  
   "EP_SEARCH_ZOOM" : "?zoom=element:availability,element:code,element:definition,element:element,element:price",
-  "EP_SEARCH" : "/cortex/searches/britney/keywords/items?followlocation",
-
   "EP_PDP_ZOOM" : "?zoom=availability,code,definition,definition:options:element,definition:options:element:selector:choice:description,definition:options:element:selector:chosen:description,price",
-  
+  "EP_SHOPPING_CART_ZOOM": "?zoom=discount,lineitems,lineitems:element,lineitems:element:availability,lineitems:element:item,lineitems:element:price,lineitems:element:item:code,order,total",
+  "EP_GET_BILLING_ADDRESS_ZOOM": "/billingaddressinfo?zoom=selector:choice:description,selector:chosen:description",
+  "EP_GET_SHIPPING_ADDRESS_ZOOM": "/deliveries?zoom=element:destinationinfo:selector:chosen:description,element:destinationinfo:selector:choice:description",
+  "EP_SHIPMODE_ZOOM" : "?zoom=order:deliveries:element:shippingoptioninfo:selector:choice:description,order:deliveries:element:shippingoptioninfo:selector:chosen:description",  
+  "EP_GET_SHIPPING_ADDRESS_SELECTOR_ZOOM" : "/deliveries?zoom=element:destinationinfo:selector",
+  "EP_WHISHLIST_CART_ZOOM" :"",
+
+
+
+  // All Custome Message Goes Here
   "EP_PRODUCT_ADDED" : "Product Added Successfully",
   "EP_ADDRESS_ADDED" : "Address Added Successfully",
   "EP_ADDRESS_DELETED" : "Address Deleted Successfully",
   "EP_ADDRESS_UPDATED"  : "Address Updated Successfully",
   "EP_ADDRESS_SELECTED" : "Address Selected Successfully",
-
-  "EP_AWS_IMAGE_PATH" : "http://s3.ap-south-1.amazonaws.com/sirius-ep-images/",
-  "EP_IMAGE_FMT": ".jpg",
-
-  "EP_LOG_DIR" : "log/elasticPath/trace.log",
-  "EP_SHOPPING_CART" : "/carts/britney/default",
-  "EP_SHOPPING_CART_ZOOM": "?zoom=discount,lineitems,lineitems:element,lineitems:element:availability,lineitems:element:item,lineitems:element:price,lineitems:element:item:code,order,total",
-  
-  "EP_CART" : "/carts/britney/default",
   "EP_SHIPMODE_SELECTED" : "Ship mode updated successfully",
-  "EP_SHIPMODE_ZOOM" : "?zoom=order:deliveries:element:shippingoptioninfo:selector:choice:description,order:deliveries:element:shippingoptioninfo:selector:chosen:description",
-  "EP_ADD_SHIPPING_ADDRESS" : "/cortex/addresses/britney?followlocation",  
-  "EP_DEFAULT_CART" : "/cortex/carts/britney/default",
-
-  "EP_GET_SHIPPING_ADDRESS_ZOOM": "/deliveries?zoom=element:destinationinfo:selector:chosen:description,element:destinationinfo:selector:choice:description",  
-  "EP_GET_SHIPPING_ADDRESS_SELECTOR_ZOOM" : "/deliveries?zoom=element:destinationinfo:selector",
-
-  "EP_GET_BILLING_ADDRESS_ZOOM": "/billingaddressinfo?zoom=selector:choice:description,selector:chosen:description",
-
-  "EP_GET_BILLING_ADDRESS_SELECTOR": "/billingaddressinfo/selector",
-
-  "EP_FOLLOW_LOCATION": "?followlocation",
+ 
   
-  "HTML_DIR" : "WebContent/html/",
 
-  
-  "EP_CURRENT_ORDER" : "/orders/britney/g5qwknzvgq4wkljqgfrtaljuga4dgllbmqytcljqmjstsntbg5rtqojsgq=",
-  "EP_ADDRESS_ADDED" : "Address Added Successfully",
-
-  "EP_WHISHLIST": "/wishlists",
-  "EP_FORM": "/form",
-  
   //constants that needs to removed & got from the app
   "EP_ACCESS_TOKEN" : "9a467a10-8652-43d2-b943-31bab6efe660"
 
---
+++ constants/elasticPath/storeConstants.js
@@ -0,0 +1,5 @@
+export default {
+      "EP_STORE" : "britney",
+      "EP_HOSTNAME" : "4c00070e.ngrok.io",
+      "EP_HOSTNAME_CORTEX" : "4c00070e.ngrok.io/cortex",
+}
