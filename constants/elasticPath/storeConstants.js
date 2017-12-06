83ecdb3f17520e5266e908e3533de7f72e8cb478
Joseph <josephpeter.john@siriuscom.com>
2017-12-06 11:28:12 +0530

    EP : Ngrok config Change

--- constants/elasticPath/storeConstants.js
+++ constants/elasticPath/storeConstants.js
@@ -1,6 +1,6 @@
 export default {
       "EP_STORE" : "britney",
-      "EP_HOSTNAME" : "4c00070e.ngrok.io",
-      "EP_HOSTNAME_CORTEX" : "4c00070e.ngrok.io/cortex",
-      "EP_ACCESS_TOKEN" : "9a467a10-8652-43d2-b943-31bab6efe660"      
+      "EP_HOSTNAME" : "5d43c440.ngrok.io",
+      "EP_HOSTNAME_CORTEX" : "5d43c440.ngrok.io/cortex",
+      "EP_ACCESS_TOKEN" : "6612452a-bca5-4722-85ea-8f14d51f4fc5"      
 };
