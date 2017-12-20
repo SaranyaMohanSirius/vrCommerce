c711daee93a67b10dc674904d2b0784492591225
Veeraraghavan Ramachandran <Veeraraghavan.Ramachandran@siriuscom.com>
2017-12-20 16:47:41 +0530

    Ngrok changes

--- constants/elasticPath/storeConstants.js
+++ constants/elasticPath/storeConstants.js
@@ -1,6 +1,6 @@
 export default {
       "EP_STORE" : "britney",
-      "EP_HOSTNAME" : "6e210a8c.ngrok.io",
-      "EP_HOSTNAME_CORTEX" : "6e210a8c.ngrok.io/cortex",
+      "EP_HOSTNAME" : "9ea9a6b0.ngrok.io",
+      "EP_HOSTNAME_CORTEX" : "9ea9a6b0.ngrok.io/cortex",
       "EP_TOKEN_EXPIRATION_TIME" : "1800000"     
 };
