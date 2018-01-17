05ac15c564ebd48da35ff149c8bb464a15c20883
Kokila Elangovan <Kokila.Elangovan@siriuscom.com>
2018-01-17 19:38:09 +0530

    removed multiple domain

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 //To Allow Cross Domain
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://project-c-web-app.herokuapp.com,https://project-c-web-app.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', 'https://project-c-web-app.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
