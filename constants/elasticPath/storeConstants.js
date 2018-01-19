c3ff117e331e752eef2e5cb2c7774f5df346d3a7
Veeraraghavan Ramachandran <Veeraraghavan.Ramachandran@siriuscom.com>
2018-01-19 14:59:53 +0530

    EP AWS instance launched changes

--- constants/elasticPath/storeConstants.js
+++ constants/elasticPath/storeConstants.js
@@ -1,6 +1,6 @@
 export default {
       "EP_STORE" : "britney",
-      "EP_HOSTNAME" : "9ea9a6b0.ngrok.io",
-      "EP_HOSTNAME_CORTEX" : "9ea9a6b0.ngrok.io/cortex",
+      "EP_HOSTNAME" : "34.216.223.217:9080",
+      "EP_HOSTNAME_CORTEX" : "34.216.223.217:9080/cortex",
       "EP_TOKEN_EXPIRATION_TIME" : "1800000"     
 };
