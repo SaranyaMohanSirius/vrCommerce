afb66a2f61ca39334a0869097f7328fab9303310
Monica Ramireddi <Monica.Ramireddi@siriuscom.com>
2018-02-01 15:59:07 +0530

    adding localhost to CORS

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 //To Allow Cross Domain
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://project-c-web-app.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', 'https://project-c-web-app.herokuapp.com, http://localhost:4200');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Cookie');
     res.header('Access-Control-Allow-Credentials', true);
