8757870b07c05f7363a0b6982450a30cab91a93a
Kokila Elangovan <Kokila.Elangovan@siriuscom.com>
2018-04-06 03:31:33 +0530

    Removed unwanted mapping in json

--- json_mappers/wcs/pdpMapper.js
+++ json_mappers/wcs/pdpMapper.js
@@ -3,181 +3,57 @@ import constants from '../../constants/wcs/constants';
 
 export default {
 
-  /* 
-   * JSON Mapper for generating responses for PDP page 
+  /*
+   * JSON Mapper for generating responses for PDP page
    */
 
-   mapPdpJSON: function(body,inv){	
+   mapPdpJSON: function(body,inv){
 		let converter = JM.makeConverter({
-			catalogEntryView: ['catalogEntryView', JM.map({		
-				hasSingleSKU: function(input){ 
-					if(inv == true){
-						return input.hasSingleSKU;
-					}
-					return; 
-				},
-				uniqueId: 'uniqueID',
-				parentCatalogGroupID: 'parentCatalogGroupID',
-				catalogEntryTypeCode: 'catalogEntryTypeCode',				
-				buyable: 'buyable',				
-				store: 'storeID',			
-				listPrice : 'price.0.value',			
-				purchasePrice: 'price.1.value',			
-				code: 'partNumber',			
-				resourceId : 'resourceId',			
+			catalogEntryView: ['catalogEntryView', JM.map({
+
+				listPrice : 'price.0.value',
+				purchasePrice: 'price.1.value',
 				displayName: 'name',
-				manufacturer: 'manufacturer',
-				seoKeyword: 'seo_token_ntk',
-				attributes: ['attributes', JM.map({
-					  displayable: 'displayable',
-					  name: 'name',
-					  identifier: 'identifier',
-					  values: ['values', JM.map({
-					  	value: 'value',
-						identifier: 'identifier',
-                        uniqueID: 'uniqueID',
-                        image : ['image1path',function(url){ 
-                          	if(url){
-                          		return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
-                      		}
-                      	}],
-					  })],
-					  usage: 'usage',
-					  sequence : 'sequence',
-				})],
-				merchandisingAssociations: ['merchandisingAssociations', JM.map({
-					hasSingleSKU: 'hasSingleSKU',
-					catalogEntryTypeCode: 'catalogEntryTypeCode',				
-					buyable: 'buyable',				
-					store: 'storeID',		
-					uniqueID: 'uniqueID',	
-					listPrice : 'price.0.value',			
-					purchasePrice: 'price.1.value',			
-					code: 'partNumber',			
-					resourceId : 'resourceId',			
-					displayName: 'name',	
-					seoKeyword: 'seo_token_ntk',
-					attributes: ['attributes', JM.map({
-						  displayable: 'displayable',
-						  name: 'name',
-						  identifier: 'identifier',
-						  values: ['values', JM.map({
-							identifier: 'identifier',
-							uniqueID: 'uniqueID',
-							image : ['image1path',function(url){ 
-                          		if(url){
-                          			return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
-                      			}
-                      		}],
-						  })], 
-					})],
-					
-                    thumbnail : ['thumbnail',function(url){ 
-                        if(url){
-                          	return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
-                      	}
-                     }],
-                    fullImage : ['fullImage',function(url){ 
-                        if(url){
-                          	return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
-                      	}
-                     }],		
-				})],
-				skus: ['sKUs',JM.map({
-					skuId: 'uniqueID',
-					hasSingleSKU: 'hasSingleSKU',
-					catalogEntryTypeCode: 'catalogEntryTypeCode',				
-					buyable: 'buyable',				
-					store: 'storeID',			
-					listPrice : 'price.0.value',			
-					purchasePrice: 'price.1.value',			
-					code: 'partNumber',			
-					resourceId : 'resourceId',			
-					displayName: 'name',
-					seoKeyword: 'seo_token_ntk',
-					attributes: ['attributes', JM.map({
-					  displayable: 'displayable',
-					  name: 'name',
-					  identifier: 'identifier',
-					  values: ['values', JM.map({
-						identifier: 'identifier',
-                        uniqueID: 'uniqueID',
-                        image : ['image1path',function(url){ 
-                          	if(url){
-                          		return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
-                      		}
-                      	}],
-					  })],
-					  usage: 'usage',
-					  sequence : 'sequence',
-					})],
-					attachments:['attachments', JM.map({
-						attachmentAssetPath : ['attachmentAssetPath',function(url){ 
-							if(url){
-							  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
-							  }
-						 }],
-						usage: 'usage',
-					})],
-					thumbnail : ['thumbnail',function(url){ 
-                        if(url){
-                          return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
-                      	}
-                     }],
-				})],
-				thumbnail : ['thumbnail',function(url){ 
+				fullImage : ['fullImage',function(url){
                     if(url){
-                          return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
-                     }
-                }],
-				fullImage : ['fullImage',function(url){ 
-                    if(url){
-                          return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                          return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                     }
-				}],
-				attachments:['attachments', JM.map({
-					attachmentAssetPath : ['attachmentAssetPath',function(url){ 
-						if(url){
-						  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
-						  }
-					 }],
-					usage: 'usage',
-				})],
-			})],	
-		});    
+				}]
+
+		});
 		let result = converter(body);
 		let jsonObj = result;
-		
+
 		if(inv != true){
 			for(let i=0 ; i<jsonObj.catalogEntryView.length ; i++){
-				let invAvailability = inv.InventoryAvailability[i].inventoryStatus;	
-				let quantityAvailable = inv.InventoryAvailability[i].availableQuantity;	
-				
+				let invAvailability = inv.InventoryAvailability[i].inventoryStatus;
+				let quantityAvailable = inv.InventoryAvailability[i].availableQuantity;
+
 				jsonObj.catalogEntryView[i].availability = invAvailability;
 				jsonObj.catalogEntryView[i].quantity = quantityAvailable;
 			}
-		}	
+		}
         return jsonObj;
   },
 
-  /* 
+  /*
    * JSON Mapper for generating responses for Related products
    */
 
-   mapRelatedProductsJSON: function(body,inv){	
+   mapRelatedProductsJSON: function(body,inv){
 		let converter = JM.makeConverter({
-			catalogEntryView: ['catalogEntryView', JM.map({		
+			catalogEntryView: ['catalogEntryView', JM.map({
 				merchandisingAssociations: ['merchandisingAssociations', JM.map({
 					hasSingleSKU: 'hasSingleSKU',
-					catalogEntryTypeCode: 'catalogEntryTypeCode',				
-					buyable: 'buyable',				
-					store: 'storeID',		
-					uniqueID: 'uniqueID',	
-					listPrice : 'price.0.value',			
-					purchasePrice: 'price.1.value',			
-					code: 'partNumber',			
-					resourceId : 'resourceId',			
-					displayName: 'name',	
+					catalogEntryTypeCode: 'catalogEntryTypeCode',
+					buyable: 'buyable',
+					store: 'storeID',
+					uniqueID: 'uniqueID',
+					listPrice : 'price.0.value',
+					purchasePrice: 'price.1.value',
+					code: 'partNumber',
+					resourceId : 'resourceId',
+					displayName: 'name',
 					seoKeyword: 'seo_token_ntk',
 					attributes: ['attributes', JM.map({
 						  displayable: 'displayable',
@@ -186,58 +62,58 @@ export default {
 						  values: ['values', JM.map({
 							identifier: 'identifier',
 							uniqueID: 'uniqueID',
-							image : ['image1path',function(url){ 
+							image : ['image1path',function(url){
                           		if(url){
-                          			return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                          			return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                       			}
                       		}],
-						  })], 
+						  })],
 					})],
-					
-                    thumbnail : ['thumbnail',function(url){ 
+
+                    thumbnail : ['thumbnail',function(url){
                         if(url){
-                          	return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                          	return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                       	}
                      }],
-                    fullImage : ['fullImage',function(url){ 
+                    fullImage : ['fullImage',function(url){
                         if(url){
-                          	return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                          	return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                       	}
-                     }],		
+                     }],
 				})],
-			})],	
-		});    
+			})],
+		});
 		let result = converter(body);
 		let jsonObj = result;
-		
+
 		if(inv != true){
 			for(let i=0 ; i<jsonObj.catalogEntryView.length ; i++){
-				let invAvailability = inv.InventoryAvailability[i].inventoryStatus;	
-				let quantityAvailable = inv.InventoryAvailability[i].availableQuantity;	
-				
+				let invAvailability = inv.InventoryAvailability[i].inventoryStatus;
+				let quantityAvailable = inv.InventoryAvailability[i].availableQuantity;
+
 				jsonObj.catalogEntryView[i].availability = invAvailability;
 				jsonObj.catalogEntryView[i].quantity = quantityAvailable;
 			}
-		}	
+		}
         return jsonObj;
   },
-    /* 
-   * JSON Mapper for generating responses for Quick View page 
+    /*
+   * JSON Mapper for generating responses for Quick View page
    */
 
-  mapQuickViewJSON: function(body,inv){	
+  mapQuickViewJSON: function(body,inv){
 		let converter = JM.makeConverter({
-			catalogEntryView: ['catalogEntryView', JM.map({		
+			catalogEntryView: ['catalogEntryView', JM.map({
 				hasSingleSKU: 'hasSingleSKU',
 				uniqueId: 'uniqueID',
-				catalogEntryTypeCode: 'catalogEntryTypeCode',				
-				buyable: 'buyable',				
-				store: 'storeID',			
-				listPrice : 'price.0.value',			
-				purchasePrice: 'price.1.value',			
-				code: 'partNumber',			
-				resourceId : 'resourceId',			
-				displayName: 'name',	
+				catalogEntryTypeCode: 'catalogEntryTypeCode',
+				buyable: 'buyable',
+				store: 'storeID',
+				listPrice : 'price.0.value',
+				purchasePrice: 'price.1.value',
+				code: 'partNumber',
+				resourceId : 'resourceId',
+				displayName: 'name',
 				seoKeyword: 'seo_token_ntk',
 				attributes: ['attributes', JM.map({
 					  displayable: 'displayable',
@@ -246,23 +122,23 @@ export default {
 					  values: ['values', JM.map({
 						identifier: 'identifier',
                         uniqueID: 'uniqueID',
-                        image : ['image1path',function(url){ 
+                        image : ['image1path',function(url){
                           if(url){
-                          		return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                          		return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                       		}
                       	}],
-					  })], 
+					  })],
 				})],
 				skus: ['sKUs',JM.map({
 					skuId:'uniqueID',
 					hasSingleSKU: 'hasSingleSKU',
-					catalogEntryTypeCode: 'catalogEntryTypeCode',				
-					buyable: 'buyable',				
-					store: 'storeID',			
-					listPrice : 'price.0.value',			
-					purchasePrice: 'price.1.value',			
-					code: 'partNumber',			
-					resourceId : 'resourceId',			
+					catalogEntryTypeCode: 'catalogEntryTypeCode',
+					buyable: 'buyable',
+					store: 'storeID',
+					listPrice : 'price.0.value',
+					purchasePrice: 'price.1.value',
+					code: 'partNumber',
+					resourceId : 'resourceId',
 					displayName: 'name',
 					seoKeyword: 'seo_token_ntk',
 					attributes: ['attributes', JM.map({
@@ -272,61 +148,61 @@ export default {
 					  values: ['values', JM.map({
 						identifier: 'identifier',
                         uniqueID: 'uniqueID',
-                        image : ['image1path',function(url){ 
+                        image : ['image1path',function(url){
                           if(url){
-                          		return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                          		return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                       		}
                       	}],
-					  })], 	
+					  })],
 					})],
 					attachments:['attachments', JM.map({
-						attachmentAssetPath : ['attachmentAssetPath',function(url){ 
+						attachmentAssetPath : ['attachmentAssetPath',function(url){
 							if(url){
-							  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+							  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
 							  }
 						 }],
 						usage: 'usage',
 					})],
-					thumbnail : ['thumbnail',function(url){ 
+					thumbnail : ['thumbnail',function(url){
                         if(url){
-                          	return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                          	return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                       	}
                      }],
 				})],
-				thumbnail : ['thumbnail',function(url){ 
+				thumbnail : ['thumbnail',function(url){
                     if(url){
-                          return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                          return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                     }
                 }],
-				fullImage : ['fullImage',function(url){ 
+				fullImage : ['fullImage',function(url){
                     if(url){
-                         return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                         return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                   	}
 				}],
 				attachments:['attachments', JM.map({
-					attachmentAssetPath : ['attachmentAssetPath',function(url){ 
+					attachmentAssetPath : ['attachmentAssetPath',function(url){
 						if(url){
-						  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+						  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
 						  }
 					 }],
 					usage: 'usage',
 				})],
-			})],	
-		});    
+			})],
+		});
 		let result = converter(body);
 		let jsonObj = result;
-		
+
 		for(let i=0 ; i<jsonObj.catalogEntryView.length ; i++){
-			let invAvailability = inv.InventoryAvailability[i].inventoryStatus;	
-			let quantityAvailable = inv.InventoryAvailability[i].availableQuantity;	
-			
+			let invAvailability = inv.InventoryAvailability[i].inventoryStatus;
+			let quantityAvailable = inv.InventoryAvailability[i].availableQuantity;
+
 			jsonObj.catalogEntryView[i].availability = invAvailability;
 			jsonObj.catalogEntryView[i].quantity = quantityAvailable;
-		}	
+		}
         return jsonObj;
   },
 
-  /* 
+  /*
    * JSON Mapper for generating responses for Recently viewed items
    */
   mapRecentlyViewedProductsJSON: function(body){
@@ -341,9 +217,9 @@ export default {
 			listPrice: 'listPrice',
 			seoKeyword: 'seo_token_ntk',
 			description: ['description', JM.map({
-				thumbnail : ['thumbnail',function(url){ 
+				thumbnail : ['thumbnail',function(url){
                   if(url){
-                        return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+                        return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
                    }
                 }],
                 shortDescription: 'shortDescription',
@@ -360,24 +236,24 @@ export default {
 	  });
 	  let result = converter(body);
 	  return result;
-  },        
+  },
 
   mapProductDetailBySingleIdJSON: function(body){
 
 
 
 	let converter = JM.makeConverter({
-    	catalogEntryView: ['catalogEntryView', JM.map({		
+    	catalogEntryView: ['catalogEntryView', JM.map({
 			hasSingleSKU: 'hasSingleSKU',
 			uniqueId: 'uniqueID',
-			catalogEntryTypeCode: 'catalogEntryTypeCode',				
-			buyable: 'buyable',				
-			store: 'storeID',			
-			listPrice : 'price.0.value',			
-			purchasePrice: 'price.1.value',			
-			code: 'partNumber',			
-			resourceId : 'resourceId',			
-			displayName: 'name',	
+			catalogEntryTypeCode: 'catalogEntryTypeCode',
+			buyable: 'buyable',
+			store: 'storeID',
+			listPrice : 'price.0.value',
+			purchasePrice: 'price.1.value',
+			code: 'partNumber',
+			resourceId : 'resourceId',
+			displayName: 'name',
 			seoKeyword: 'seo_token_ntk',
 			attributes: ['attributes', JM.map({
 				  displayable: 'displayable',
@@ -386,23 +262,23 @@ export default {
 				  values: ['values', JM.map({
 					identifier: 'identifier',
 					uniqueID: 'uniqueID',
-					image : ['image1path',function(url){ 
+					image : ['image1path',function(url){
 					  if(url){
-							  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+							  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
 						  }
 					  }],
-				  })], 
+				  })],
 			})],
 			skus: ['sKUs',JM.map({
 				skuId:'uniqueID',
 				hasSingleSKU: 'hasSingleSKU',
-				catalogEntryTypeCode: 'catalogEntryTypeCode',				
-				buyable: 'buyable',				
-				store: 'storeID',			
-				listPrice : 'price.0.value',			
-				purchasePrice: 'price.1.value',			
-				code: 'partNumber',			
-				resourceId : 'resourceId',			
+				catalogEntryTypeCode: 'catalogEntryTypeCode',
+				buyable: 'buyable',
+				store: 'storeID',
+				listPrice : 'price.0.value',
+				purchasePrice: 'price.1.value',
+				code: 'partNumber',
+				resourceId : 'resourceId',
 				displayName: 'name',
 				seoKeyword: 'seo_token_ntk',
 				attributes: ['attributes', JM.map({
@@ -412,50 +288,50 @@ export default {
 				  values: ['values', JM.map({
 					identifier: 'identifier',
 					uniqueID: 'uniqueID',
-					image : ['image1path',function(url){ 
+					image : ['image1path',function(url){
 					  if(url){
-							  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+							  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
 						  }
 					  }],
-				  })], 	
+				  })],
 				})],
-				thumbnail : ['thumbnail',function(url){ 
+				thumbnail : ['thumbnail',function(url){
 					if(url){
-						  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+						  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
 					  }
 				 }],
 				 attachments:['attachments', JM.map({
-					attachmentAssetPath : ['attachmentAssetPath',function(url){ 
+					attachmentAssetPath : ['attachmentAssetPath',function(url){
 						if(url){
-						  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+						  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
 						  }
 					 }],
 					usage: 'usage',
 				})],
 			})],
-			thumbnail : ['thumbnail',function(url){ 
+			thumbnail : ['thumbnail',function(url){
 				if(url){
-					  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+					  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
 				}
 			}],
-			fullImage : ['fullImage',function(url){ 
+			fullImage : ['fullImage',function(url){
 				if(url){
-					 return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+					 return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
 				  }
 			}],
 			attachments:['attachments', JM.map({
-				attachmentAssetPath : ['attachmentAssetPath',function(url){ 
+				attachmentAssetPath : ['attachmentAssetPath',function(url){
 					if(url){
-					  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url); 
+					  return (constants.WCS_DOUBLE_SLASH+constants.WCS_HOSTNAME_NOPORT+url);
 					  }
 				 }],
 				usage: 'usage',
 			})],
 		})],
-		
+
 
 	});
 	let result = converter(body);
 	return result;
-}        
+}
 };
